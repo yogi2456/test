a5fd16e2e4b92ab295b75b064c3cb527fb8b8c7b
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-09-02 17:20:10 +0530

    updates

--- test/test1.js
+++ test/test1.js
@@ -1,10 +1,13 @@
-var array = [2, 3, 5, 4, 6, 7, 8, 45, 34, 78, 23];
-result = 0;
+// var array = [2, 3, 5, 4, 6, 7, 8, 45, 34, 78, 23];
+// result = 0;
 
-function sumofallelements(array) {
-    for (var i = 0; i < array.length; i++) {
-        result = result + array[i]
-    }
-    return result;
-}
-console.log(sumofallelements(array))
+// function sumofallelements(array) {
+//     for (var i = 0; i < array.length; i++) {
+//         result = result + array[i]
+//     }
+//     return result;
+// }
+// console.log(sumofallelements(array))
+
+var name = "yogesh"
+console.log(name)
