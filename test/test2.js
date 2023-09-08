2303a2d6eeed80b4e7204b424ccb493653b91e79
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-09-08 13:25:04 +0530

    updates

--- test/test2.js
+++ test/test2.js
@@ -6,33 +6,33 @@
 //***
 //****
 //*****
+var input = 5;
 
-// var input = 5;
+function printstars(number) {
 
-// function printstars(number) {
+    for(i = 1; i <= number; i++) {
+        var pattern = ""
+        for (j = 1; j <= i; j++) {
+            pattern = pattern + "*"
+        }
+        console.log(pattern)
+    }
+}
 
-//     for(i = 1; i <= number; i++) {
-//         var pattern = ""
-//         for (j = 1; j <= i; j++) {
-//             pattern = pattern + "*"
-//         }
-//         console.log(pattern)
-//     }
-// }
-
-// printstars(input)
+printstars(input)
 
 // Palindrome Check
 // Write a JavaScript function that checks if a given string is a palindrome (reads the same backward as forward). Return true if it's a palindrome; otherwise, return false.
-
 // Example:
-
 // Input: "racecar"
 // Output: true
 
 var string = "abcdcba";
 
-function palindromecheck(str) {
+function checkp(str) {
+    if(str.length < 2) {
+        return "str not valid"
+    }
     for (var i = 0; i < str.length / 2; i++) {
         //console.log(array[i])
         if (str[i] !== str[str.length - 1 - i]) {
@@ -42,4 +42,22 @@ function palindromecheck(str) {
     return true
 }
 
-console.log(palindromecheck(str))
+console.log(checkp(string))
+
+var string1 = "a";
+var flag = false;
+
+function palindromecheck(str1) {
+
+    for (var i = 0; (i < str1.length / 2) && (str1.length > 1); i++) {
+        if (str1[i] !== str1[str1.length - 1 - i]) {
+            return false
+        }
+        flag = true
+    }
+    return flag
+}
+
+console.log(palindromecheck(string1))
+
+
