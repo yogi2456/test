159508c04100e678d72d957b1452ce80b4c76f2b
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-11 11:35:19 +0530

    updates

--- test/Q4.js
+++ test/Q4.js
@@ -76,7 +76,7 @@ var array1 = [34, 23, 54, 3, 5, 22, 1];
 function findtwoarrays(array, array1) {
     var result = [];
     for (var i = 0; i < array.length; i++) {
-        for (var j = 0; j < array1.length; i++) {
+        for (var j = 0; j < array1.length; j++) {
             //console.log(array, array1)
             //console.log(i,j)
             //console.log(array[i], array1[j])
--- test/test2.js
+++ test/test2.js
@@ -1,64 +1,64 @@
-// // create a program that prints the following pattern using astericks using for loops:
-// //input -5
-// //output -
-// //*
-// //**
-// //***
-// //****
-// //*****
-// var input = 5;
+// create a program that prints the following pattern using astericks using for loops:
+//input -5
+//output -
+//*
+//**
+//***
+//****
+//*****
+var input = 5;
 
-// function printstars(number) {
+function printstars(number) {
 
-//     for(i = 1; i <= number; i++) {
-//         var pattern = ""
-//         for (j = 1; j <= i; j++) {
-//             pattern = pattern + "*"
-//         }
-//         console.log(pattern)
-//     }
-// }
+    for(i = 1; i <= number; i++) {
+        var pattern = ""
+        for (j = 1; j <= i; j++) {
+            pattern = pattern + "*"
+        }
+        console.log(pattern)
+    }
+}
 
-// printstars(input)
+printstars(input)
 
-// // Palindrome Check
-// // Write a JavaScript function that checks if a given string is a palindrome (reads the same backward as forward). Return true if it's a palindrome; otherwise, return false.
-// // Example:
-// // Input: "racecar"
-// // Output: true
+// Palindrome Check
+// Write a JavaScript function that checks if a given string is a palindrome (reads the same backward as forward). Return true if it's a palindrome; otherwise, return false.
+// Example:
+// Input: "racecar"
+// Output: true
 
-// var string = "abcdcba";
+var string = "abcdcba";
 
-// function checkp(str) {
-//     if(str.length < 2) {
-//         return "str not valid"
-//     }
-//     for (var i = 0; i < str.length / 2; i++) {
-//         //console.log(array[i])
-//         if (str[i] !== str[str.length - 1 - i]) {
-//             return false
-//         }
-//     }
-//     return true
-// }
+function checkp(str) {
+    if(str.length < 2) {
+        return "str not valid"
+    }
+    for (var i = 0; i < str.length / 2; i++) {
+        //console.log(array[i])
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
 
-// console.log(checkp(string))
+console.log(checkp(string))
 
-// var string1 = "a";
-// var flag = false;
+var string1 = "a";
+var flag = false;
 
-// function palindromecheck(str1) {
+function palindromecheck(str1) {
 
-//     for (var i = 0; (i < str1.length / 2) && (str1.length > 1); i++) {
-//         if (str1[i] !== str1[str1.length - 1 - i]) {
-//             return false
-//         }
-//         flag = true
-//     }
-//     return flag
-// }
+    for (var i = 0; (i < str1.length / 2) && (str1.length > 1); i++) {
+        if (str1[i] !== str1[str1.length - 1 - i]) {
+            return false
+        }
+        flag = true
+    }
+    return flag
+}
 
-// console.log(palindromecheck(string1))
+console.log(palindromecheck(string1))
 
 
 var num = [2, 7, 11, 15]
@@ -76,3 +76,23 @@ for (var i = 0; i < num.length; i++) {
         }
     }
 }
+
+var array = [1, 3, 5, 67, 45, 87];
+var array1 = [34, 23, 54, 3, 5, 22, 1];
+
+function findtwoarrays(array, array1) {
+    var result = [];
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array1.length; j++) {
+            //console.log(array, array1)
+            //console.log(i,j)
+            //console.log(array[i], array1[j])
+            if (array[i] == array1[j]) {
+                result.push(array[i])
+            }
+        }
+    }
+    return result
+}
+
+console.log(findtwoarrays(array, array1))
