725badb3ab88aef4e31a1a04971fece1fa9c64d1
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-27 22:25:31 +0530

    updates

--- test3/Q3.js
+++ test3/Q3.js
@@ -1,7 +1,7 @@
 var x = 123;
 
 function palindrome(x) {
-    for( var i = 0; i < x.length; i++) {
+    for( var i = 0; i < x.length/2; i++) {
         if (x[i] == x[x.length -1]) {
             return false;
         }
